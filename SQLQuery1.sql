bc211023785087d047107f7f250f42878724171e
David Billups <davidtbillups@gmail.com>
2021-05-06 15:23:20 -0400

    committing to check prs

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,15 +1,3 @@
-﻿SELECT p.id,
-       p.Title As PostTitle,
-       p.URL AS PostUrl,
-       p.PublishDateTime,
-       p.AuthorId,
-       p.BlogId,
-       a.FirstName,
-       a.LastName,
-       a.Bio,
-       b.Title AS BlogTitle,
-       b.URL AS BlogUrl
-FROM Post p 
-LEFT JOIN Author a on p.AuthorId = a.Id
-LEFT JOIN Blog b on p.BlogId = b.Id
+﻿SELECT * FROM Post
+
 
