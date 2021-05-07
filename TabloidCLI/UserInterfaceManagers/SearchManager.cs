5cc148ded334d6b79b0127592fe13b7f7762463c
David Billups <davidtbillups@gmail.com>
2021-05-07 14:18:12 -0400

    committing to check Matts PR

--- TabloidCLI/UserInterfaceManagers/SearchManager.cs
+++ TabloidCLI/UserInterfaceManagers/SearchManager.cs
@@ -17,26 +17,36 @@ namespace TabloidCLI.UserInterfaceManagers
         public IUserInterfaceManager Execute()
         {
             Console.WriteLine("Search Menu");
-            Console.WriteLine(" 1) Search Blogs");
-            Console.WriteLine(" 2) Search Authors");
+            Console.WriteLine(" 1) Search Authors");
+            Console.WriteLine(" 2) Search Blogs");
             Console.WriteLine(" 3) Search Posts");
             Console.WriteLine(" 4) Search All");
             Console.WriteLine(" 0) Go Back");
 
             Console.Write("> ");
+            string tagName;
             string choice = Console.ReadLine();
             switch (choice)
             {
                 case "1":
-                    SearchBlogs();
+                    Console.Write("Tag > ");
+                    tagName = Console.ReadLine();
+                    SearchAuthors(tagName);
                     return this;
                 case "2":
-                    SearchAuthors();
+                    Console.Write("Tag > ");
+                    tagName = Console.ReadLine();
+                    SearchBlogs(tagName);
                     return this;
                 case "3":
-                    SearchPosts();
+                    Console.Write("Tag > ");
+                    tagName = Console.ReadLine();
+                    SearchPosts(tagName);
                     return this;
                 case "4":
+                    Console.Write("Tag > ");
+                    tagName = Console.ReadLine();
+                    SearchAll(tagName);
                     return this;
                 case "0":
                     return _parentUI;
@@ -46,28 +56,8 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
-        private void SearchBlogs()
+        private void SearchAuthors(string tagName)
         {
-            Console.Write("Tag> ");
-            string tagName = Console.ReadLine();
-
-            SearchResults<Blog> results = _tagRepository.SearchBlogs(tagName);
-
-            if (results.NoResultsFound)
-            {
-                Console.WriteLine($"No results for {tagName}");
-            }
-            else
-            {
-                results.Display();
-            }
-        }
-
-        private void SearchAuthors()
-        {
-            Console.Write("Tag> ");
-            string tagName = Console.ReadLine();
-
             SearchResults<Author> results = _tagRepository.SearchAuthors(tagName);
 
             if (results.NoResultsFound)
@@ -80,11 +70,22 @@ namespace TabloidCLI.UserInterfaceManagers
             }
         }
 
-        private void SearchPosts()
+        private void SearchBlogs(string tagName)
         {
-            Console.Write("Tag> ");
-            string tagName = Console.ReadLine();
+            SearchResults<Blog> results = _tagRepository.SearchBlogs(tagName);
 
+            if (results.NoResultsFound)
+            {
+                Console.WriteLine($"No results for {tagName}");
+            }
+            else
+            {
+                results.Display();
+            }
+        }
+
+        private void SearchPosts(string tagName)
+        {
             SearchResults<Post> results = _tagRepository.SearchPosts(tagName);
 
             if (results.NoResultsFound)
@@ -95,6 +96,16 @@ namespace TabloidCLI.UserInterfaceManagers
             {
                 results.Display();
             }
+        }
+
+        private void SearchAll(string tagName)
+        {
+            Console.WriteLine("Author Results: ");
+            SearchAuthors(tagName);
+            Console.WriteLine("Blog Results: ");
+            SearchBlogs(tagName);
+            Console.WriteLine("Post Results: ");
+            SearchPosts(tagName);
         }
     }
 }
