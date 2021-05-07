39f6f5aecca2304c295a59c8f5c934a81256160d
Patti Perry <pattiperry12@gmail.com>
2021-05-07 10:45:46 -0400

    working on notes menu

---
+++ TabloidCLI/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabloidCLI.Models
+{
+    class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public Post Post { get; set; }
+
+        public List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
---
+++ TabloidCLI/Repositories/NoteRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+
+namespace TabloidCLI
+{
+    public class NoteRepository : DatabaseConnector, IRepository<Note>
+    {
+        public NoteRepository(string connectionString) : base(connectionString) { }
+
+        public List<Note> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT n.Id as noteId, n.Title, n.Content, n.CreateDateTime, p.Id as                    postId FROM Note n
+                                       Left JOIN Post p on n.PostId = p.Id";
+
+                    List<Note> notes = new List<Note>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Note note = new Note()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("noteId")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                            Id = reader.GetInt32(reader.GetOrdinal("postId")),
+                        };
+                        notes.Add(note);
+                    }
+
+                    reader.Close();
+
+                    return notes;
+                }
+            }
+        }
+
+        public Author Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Author author)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Author (FirstName, LastName, Bio )
+                                                     VALUES (@firstName, @lastName, @bio)";
+                    cmd.Parameters.AddWithValue("@firstName", author.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", author.LastName);
+                    cmd.Parameters.AddWithValue("@bio", author.Bio);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM NOte WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        
+    }
+}
